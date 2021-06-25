9c2cea12131290f23ca5f380dc8584cee8132592
João Paulo <mg_ses@hotmail.com>
2021-06-25 18:43:53 -0300

    User controller, repository,dto and auth implementation

---
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private AuthService AuthService;
+	
+	@Transactional(readOnly = true)
+	public UserDTO getProfile() {
+		User user = AuthService.authenticated();
+		return new UserDTO(user);
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+		return user;
+	}
+}
